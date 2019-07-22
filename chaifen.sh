1af2d4ea33c7f103f96e5d18c88a00dfff4e8394
zihaozhang9 <zihaozhang9@foxmail.com>
2019-07-22 18:55:50 +0800

    Update chaifen.sh

--- chaifen.sh
+++ chaifen.sh
@@ -1,13 +1,21 @@
- cat charData/training_pad/training_pad.txt charData/test_pad/test_pad.txt > all.txt
+# cat charData/training_pad/training_pad.txt charData/test_pad/test_pad.txt > all.txt
+if [ ! -d data  ];then
+   mkdir data
+ fi
+path=/data/wangzai/images/
+ls $path | sed "s:^:$path:" > all.txt
  shuf all.txt > all2.txt
  a=$(wc -l <all2.txt)
  #a,b= wc -l all2.txt#| awk '{print $1}'
- echo $a
+ echo 'all:'$a
  test=$(($a/5))
- echo $test
+ echo 'test:'$test
  train=$(($a-$test))
- echo $train
+ echo 'train:'$train
  
-head -$test all2.txt > test.txt
-tail -$train all2.txt > train.txt
+head -$test all2.txt > data/test.txt
+tail -$train all2.txt > data/train.txt
+rm all.txt
+rm all2.txt
+
  
