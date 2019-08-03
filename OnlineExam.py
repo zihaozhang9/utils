58d68cbc723f726a689e67648bff4fcd7dc09b92
zihaozhang9 <zihaozhang9@foxmail.com>
2019-08-03 13:57:44 +0800

    就算不过笔试也要优雅

--- OnlineExam.py
+++ OnlineExam.py
@@ -1,16 +1,28 @@
-import os
-import os.path as osp
 
-def decode_input(Str_in):
-	list = Str_in.strip().split()
-	try:
-		list =  [int(i) for i in list]
-	except:
-		print('input error')
-		exit()
-	return list
-
-print('input:')
-N,M = decode_input(input())
-print('out input:')
-print(N,M,type(N))
+#编程题的输入https://blog.csdn.net/m0_37134612/article/details/80763485
+def decIn(Str_in):
+    line = Str_in.strip().split()
+    try:
+        line = [int(i) for i in line]
+    except:
+        print('input error')
+        exit()
+    return line
+def read1(chang):
+    tmp = decIn(input())
+    while len(tmp)<chang:
+        tmp = decIn(input())
+    return tmp
+def readN(hang,chang):
+    tmp = []
+    while len(tmp)<hang:
+        tmp.append( read1(chang) )
+    return tmp
+N,M = read1(2)
+DP = readN(N,2)
+D = [i[0] for i in DP]
+P = [i[1] for i in DP]
+A = read1(M)
+print(N,M)
+print(D,P)
+print(A)
