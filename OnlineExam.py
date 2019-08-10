8cc5638e4d087ba0d44cc3d1ad07e2e8474ec851
zihaozhang9 <zihaozhang9@foxmail.com>
2019-08-10 22:11:43 +0800

    Update OnlineExam.py

--- OnlineExam.py
+++ OnlineExam.py
@@ -58,3 +58,92 @@ while 1:
 
 #赛码网别人的输入方式
 nm = list(map(int,input().split(" ")))
+
+
+
+#depth_first_search
+#breadth_first_search
+#赛码网例题https://discuss.acmcoder.com/topic/5d47dfa8b99ad44605a1700b
+#csdn示例https://blog.csdn.net/changyuanchn/article/details/79008760
+
+#      1
+#  （2     3）
+# （4 5）（6 7）
+# （8）
+#一种图结构
+#dfs：12485367
+#bfs: 12345678
+#因为1连接的边23不会有重复
+#在python中用dict{set(),set(),set()}的格式存储最好
+
+'''
+    -图的遍历
+    初始化：要有图的结构变量
+    添加边功能
+    （所有节点都有边连接关系，不会有不连接的，所以不需要添加节点功能）
+    广度优先搜索功能bfs:
+        非递归方式：
+            queue负责下次遍历的节点
+        递归方式：
+            queue下一次遍历的节点
+            
+    深度优先搜索功能dfs:非递归方式
+                        递归方式
+'''
+class Graph:
+    def __init__(self):
+        self.edge={}
+    def addEdge(self,a,b):
+        if not a in self.edge.keys():
+            self.edge[a] = set()
+        self.edge[a].add(b)
+    def bfs(self,node):#非递归，循环方式
+        queue,order = [], []
+        queue.append(node)
+        order.append(node)
+        while queue:
+            v = queue.pop(0)
+            for vv in self.edge[v]:
+                if vv not in order:
+                    order.append(vv)
+                    queue.append(vv)
+        return order
+    def bfs2(self,node,order=[],queue=[]):#递归方式
+        if not type(node)==list : node = [node]
+        for n in node:
+            if not n in order:
+                order.append(n)
+                for v in self.edge[n]:
+                    queue.append(v)
+        if len(queue)>0:self.bfs2(queue,order,[])
+        return order
+    #dfs非递归，太难实现    
+    def dfs(self, node, order=[]): #dfs 递归方式
+        order.append(node)
+        for v in self.edge[node]:
+            if not v in order:
+                self.dfs(v, order)
+        return order
+        
+#Test Case
+
+def main(): 
+    sides=[(1, 2),
+        (1, 3),
+        (2, 4),
+        (2, 5),
+        (4, 8),
+        (5, 8),
+        (3, 6),
+        (3, 7),
+        (6, 7)]
+    g = Graph()
+    for s in sides:
+        g.addEdge(s[0],s[1])
+        g.addEdge(s[1],s[0])
+    #print(g.bfs(1))
+    #import pdb;pdb.set_trace()
+    #print(g.bfs2(1))
+    print(g.dfs(1))
+if __name__ == "__main__":
+    main() 
