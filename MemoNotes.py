4e19b0142b08bc93eb96ffb6174c65ead83271c6
zihaozhang9 <zihaozhang9@foxmail.com>
2020-11-26 23:12:32 +0800

    Create MemoNotes.py

---
+++ MemoNotes.py
@@ -0,0 +1,4 @@
+from decimal import Decimal
+a = Decimal('4.1') #[python执行精确的小数计算\_杨佳佳的博客\-CSDN博客\_python 小数运算](https://blog.csdn.net/yangjiajia123456/article/details/80302615)
+b = Decimal('5.329')
+print(a+b)
